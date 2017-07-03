60f8369ef3e7b4a0b85a2f86dc81451502d677a0
Artur Czerminski <czermi@gmail.com>
2017-07-03 13:17:44 +0200

    triangle area set

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,9 @@
 // console.log('Hello, ' + name); ('The commented will not be called')
 
 
-var a = 30,
-      h = 3,
-	    triangleArea = alert ('Triangle field is ' + a*h/2);
+var a = 30;
+var h = 3;
+var triangleArea = (a * h / 2);
+alert('Triangle field is ' + triangleArea);
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
