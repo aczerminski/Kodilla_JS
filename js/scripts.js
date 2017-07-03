a9e33ee77574bd57d494b1321e156310c05b2a68
Artur Czerminski <czermi@gmail.com>
2017-07-03 07:38:03 +0200

    triangle field added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 // var name = prompt('Enter your name'); ('The commented will not be called')
 // alert('Hello, ' + name); ('The commented will not be called')
 // console.log('Hello, ' + name); ('The commented will not be called')
+
+
+var a = 30,
+      h = 3,
+	    triangleArea = alert ('Triangle field is ' + a*h/2);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
