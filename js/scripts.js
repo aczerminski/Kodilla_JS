c7f9889f3c3e6b9fd50f0f045b5cf9a092751db1
Artur Czerminski <czermi@gmail.com>
2017-07-03 14:08:46 +0200

    if conditional added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-var name = prompt('Enter your name');
+/* var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+*/
+var a = prompt ('Type a');
+var b = prompt ('Type b');
+var value = (a * a) + (2 * a * b) - (b * b);
+alert (value);
+console.log(value);
+
+if (value > 0) {
+alert('Wynik dodatni');
+} else if (value < 0) {
+alert('Wynik ujemny');
+} else {
+alert('Wynik to 0');
+}
