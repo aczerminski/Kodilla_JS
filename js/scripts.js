02d654eedd377c9d29d8347d9af4783b66c89f7f
Artur Czerminski <czermi@gmail.com>
2017-07-07 07:38:18 +0200

    function added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+// var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+// console.log('Hello, ' + name);
+
+var a = prompt('Type a');
+var h = prompt('Type h');
+function getTriangleArea(a, h) {
+if (a, h) <= 0) {
+return ('Nieprawidłowe dane');
+} if else (a, h) > 0) {
+return (a * h / 2);
+}
+}
+console.log(getTriangleArea(10, 6))
