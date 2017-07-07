a9a9ab8807e66cec309de650681476d58ec84c88
Artur Czerminski <czermi@gmail.com>
2017-07-07 11:59:28 +0200

    string added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+// var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+// console.log('Hello, ' + name);
+
+var text = ('Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.');
+var dinosaur = 'triceratops';
+var nameUpperCased = dinosaur.toUpperCase();
+var changeWord = text.replace('Velociraptor', nameUpperCased);
+console.log(changeWord.length / 2); // nie wiem jak zrobić, żeby konsola wyświetlała połowę tekstu, a nie połowę znakow
