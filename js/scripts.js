64950ce905991d414c936247a80a4fc78fc7bf58
Artur Czerminski <czermi@gmail.com>
2017-07-10 08:12:32 +0200

    dom

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+// var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+// console.log('Hello, ' + name);
+var buttonClass = document.getElementsByClassName('button');
+console.log(buttonClass);
+
+var buttons = ['Button1', 'Button2', 'Button3'];
+console.log(buttons.length);
+
+for (var buttons = 1 ; buttons <= 3 ; buttons++) {
+  var buttonText = buttons.innerText;
+  console.log(buttonText); // namotane, wiem :/ nie potrafię stworzyć instrukcji wywołującej tekst
+}
