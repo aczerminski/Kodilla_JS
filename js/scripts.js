bb1f81094781a6aed2a1f2fbd3f899c211a95ceb
Artur Czerminski <czermi@gmail.com>
2017-07-10 07:10:57 +0200

    code modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 //var name = prompt('Enter your name');
 //alert('Hello, ' + name);
 //console.log('Hello, ' + name);
+
 function drawTree(row) {
-for (i=0; i<=row; i++){
-var star = '';
-for (j=0; j<=i; j++) {
-star += '*';
+for (i = 0 ; i <= row ; i++) {
+  var star = '';
+for (j = 0 ; j <= i ; j++) {
+  star += '*';
 }
 console.log(star);
 }
