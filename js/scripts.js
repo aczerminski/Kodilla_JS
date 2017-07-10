01a87bb3ec3d4b742ac084dbbf977db284c443e8
Artur Czerminski <czermi@gmail.com>
2017-07-10 16:23:03 +0200

    code prepared

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+// var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+// console.log('Hello, ' + name);
+
+var list = document.getElementById('list');
+var add = document.getElementById('button');
+add.addEventListener('click', function() {
+    var element = document.createElement('li');
+    var newElem = document.createElement('Item 1');
+element.innerHTML = 'item';
+newElem.innerHTML = 'Item 1';
+
+});
