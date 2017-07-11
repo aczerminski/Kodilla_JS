de157362cf2d61f30967226a9cdf1af7c0a08b57
Artur Czerminski <czermi@gmail.com>
2017-07-11 08:28:52 +0200

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,9 @@
 
 var list = document.getElementById('list');
-var add = document.getElementById('button');
-addEventListener('click', function() {
+var add = document.getElementById('addElem');
+add.addEventListener('click', function() {
     var element = document.createElement('li');
-    var newElem = document.createElement('Item 1');
-    var newElem = document.createElement('Item 2');
-    var newElem = document.createElement('Item 3');
-element.innerHTML = 'Item';
-newElem.innerHTML = 'Item 1';
-newElem.innerHTML = 'Item 2';
-newElem.innerHTML = 'Item 2';
-list.appendChild(element);
+    element.innerHTML = 'Item';
+    list.appendChild(element);
 });
-// aż wstyd mi pokazywać ten kod, ale kompletnie nie wiem w ktora strone z tym pojsc :/
+// nie wiem skąd ma byc zaciagany numer biezacego elementu "li"
