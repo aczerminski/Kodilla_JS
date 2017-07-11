9ecb4105eb18ca177e5cc46eccb1f514082f6e0d
Artur Czerminski <czermi@gmail.com>
2017-07-11 08:16:39 +0200

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,6 @@
 var buttonClass = document.getElementsByClassName('button');
 console.log(buttonClass);
 
-var buttons = ['Button1', 'Button2', 'Button3'];
-console.log(buttons.length);
-
 for (var buttons = 1 ; buttons <= 3 ; buttons++) {
   var buttonText = buttons.innerText;
   console.log(buttonText); // namotane, wiem :/ nie potrafię stworzyć instrukcji wywołującej tekst
